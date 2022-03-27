96dcf56ee378905048bcab381343ecfef44e77f7
Aero385 <88101625+Aero385@users.noreply.github.com>
2022-03-27 17:13:10 +0300

    Initial commit

---
+++ src/main/java/BookStore/demo/AppController.java
@@ -0,0 +1,14 @@
+package BookStore.demo;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+    @GetMapping("")
+    public String viewHomePage(){
+        return "index";
+    }
+}
